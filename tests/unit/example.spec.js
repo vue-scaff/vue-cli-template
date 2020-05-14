81d3c6521e4e719565045dcbf36a9116786620d4
joenix <joenix@qq.com>
2020-05-14 09:51:17 +0800

    feature: update test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,12 @@
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import Example from "@/components/example.vue";
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+describe("example.vue", () => {
+  it("renders props.message when passed", () => {
+    const message = "test message";
+    const wrapper = shallowMount(Example, {
+      propsData: { message }
     });
-    expect(wrapper.text()).toMatch(msg);
+    expect(wrapper.text()).toMatch(message);
   });
 });
